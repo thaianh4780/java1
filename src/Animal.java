223887b8eb08bfe52221fdc85b11f48ded8439cb
thaianh4780 <100453973+thaianh4780@users.noreply.github.com>
2023-03-14 08:30:51 +0700

    done chương 3 và bài 5 có sài GPT

---
+++ src/Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    protected String name;
+    public Animal(String name) {
+        this.name = name;
+    }
+    public void makeSound() {
+        System.out.println("The animal makes a sound");
+    }
+}
---
+++ src/PhepChia.java
@@ -0,0 +1,16 @@
+public class PhepChia {
+    protected double tuSo ;
+    protected double mauSo;
+
+////    protected String name;
+//    public Animal(String name) {
+//        this.name = name;
+//    }
+//    public void makeSound() {
+//        System.out.println("The animal makes a sound");
+//    }
+
+//    public double PhepChia(double tuSo ,double mauSo){
+//
+//    }
+}
