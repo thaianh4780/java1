fe8b0851c420fa64d77a51a8a98eb8df3baa7c37
thaianh4780 <100453973+thaianh4780@users.noreply.github.com>
2023-03-18 08:35:20 +0700

    done bai 7

--- src/bai1/Doc_In_File.java
+++ src/bai1/Doc_In_File.java
@@ -6,18 +6,18 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Doc_In_File {
-    public static  void  doc_In_File(String fileName){
-        try{
+    public static void doc_In_File(String fileName) {
+        try {
             File file = new File(fileName);
             FileReader fileReader = new FileReader(file);
 
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line ;
-            while ((line = bufferedReader.readLine())!= null ){
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
                 System.out.println(line);
             }
-        }catch (IOException ex){
-            System.out.println("Loi doc file"+ ex);
+        } catch (IOException ex) {
+            System.out.println("Loi doc file" + ex);
         }
     }
 }
---
+++ src/bai7/Main.java
@@ -0,0 +1,14 @@
+package bai7;
+
+public class Main {
+    public static void main(String[] args) {
+//        SinhVien sinhVien = new SinhVien("1","Thai Anh",20002,"tt9");
+//        SinhVien sinhVien1 = new SinhVien("1","Thai Anh",20002,"tt9");
+//        SinhVien sinhVien2 = new SinhVien("1","Thai Anh",20002,"tt9");
+//        SinhVien sinhVien3 = new SinhVien("1","Thai Anh",20002,"tt9");
+//        SinhVien sinhVien4 = new SinhVien("1","Thai Anh",20002,"tt9");
+
+
+
+    }
+}
